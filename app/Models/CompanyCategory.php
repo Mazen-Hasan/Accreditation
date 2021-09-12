9c7138dccc76938296bdd9cc326e24acbc79ea80
Mazen <e.mazen.hasan@gmail.com>
2021-09-12 21:53:07 +0300

    CompanyCategory.php - fix model name

--- app/Models/CompanyCategory.php
+++ app/Models/CompanyCategory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class companyCategory extends Model
+class CompanyCategory extends Model
 {
     use HasFactory;
 
